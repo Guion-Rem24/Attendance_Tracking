9d9929cd46c17f400d0eee0eb349219b6c44092d
Guion-Rem24 <mne.kou24k@gmail.com>
2020-11-17 14:28:37 +0900

    modify the crash of setSupportActionBar(toolbar)

--- app/src/main/java/com/example/attendance_tracking/MainActivity.java
+++ app/src/main/java/com/example/attendance_tracking/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Main");
 
-//        setSupportActionBar(toolbar); // バグある
+        setSupportActionBar(toolbar); // バグある
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         // DrawerToggle
